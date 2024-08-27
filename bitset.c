d0d5b94751b5dd9b9b3213a917d912f35ec71d2c
jonvicbarcenas <exchantpro@gmail.com>
2024-08-27 20:55:14 +0800

    lol

--- bitset.c
+++ bitset.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "bitset.h"
 
 void initialize(BitSet s){
@@ -8,98 +9,114 @@ void initialize(BitSet s){
 }
 
 
-void add(int elem,BitSet s){
-	int i;
-	int num;
-	for(i=0; i<elem; i++){
-		s[elem] = 1;
-	}
+//void add(int elem,BitSet s){
+//	int i;
+//	int num;
+//	for(i=0; i<elem; i++){
+//		s[elem] = 1;
+//	}
+//}
+
+void add(int elem, BitSet s){
+    if (elem >= 0 && elem < MAX) {
+        s[elem] = 1;
+    }
 }
+
 
 
 void display(BitSet s){
 	int i;
 	for(i=0; i<MAX; i++){
-		printf("%d ", s[i]);
+		printf("[%d] ", s[i]);
 	}
 	
 }
 
 
-void getUnion(BitSet result,BitSet s1,BitSet s2){
-	int i;
-	for(i=0; i<MAX; i++){
-		if(s1[i] || s2[i] == 1){
-			result[i] = 1;
-		}
-	}
-	
+void getUnion(BitSet result, BitSet s1, BitSet s2) {
+    int i;
+    for (i = 0; i < MAX; i++) {
+        result[i] = s1[i] || s2[i];
+    }
 }
 
 
-void intersection(BitSet result,BitSet s1,BitSet s2){
-	int i;
-	for(i=0; i<MAX; i++){
-		if(s1[i] && s2[i] == 1){
-			result[i] = 1;
-		}
-	}
-	
+void intersection(BitSet result, BitSet s1, BitSet s2) {
+    int i;
+    for (i = 0; i < MAX; i++) {
+        result[i] = s1[i] && s2[i];
+    }
 }
 
 
-void difference(BitSet result,BitSet s1,BitSet s2){
-	int i;
-	for(i=0; i<MAX; i++){
-		if(s1[i] && !s2[i] == 1){
-			result[i] = 1;
-		}
-	}
-	
+void difference(BitSet result, BitSet s1, BitSet s2) {
+    int i;
+    for (i = 0; i < MAX; i++) {
+        result[i] = s1[i] && !s2[i];
+    }
 }
 
 
-int isEmpty(BitSet s){
-	int result=1;
-	int i;
-	for(i=0; i<MAX; i++){
-		if(s[i] == 0){
-			result[i] = 0;
-		}
-	}
-	return result; 
-	
-	// true = 0
-	// false = 1
+int isEmpty(BitSet s) {
+	int i, result = 1; //1 empty
+    for (i = 0; i < MAX; i++) {
+        if (s[i] != 0) {
+            result = 0; // Not empty
+            break;
+        }
+    }
+    return result; // Empty, 
 }
 
 
-int contains(BitSet s,int elem){
-	
-	
+int contains(BitSet s, int elem) {
+    if (elem >= 0 && elem < MAX) {
+        return s[elem];
+    }
+    return 0; // false, element not in set
 }
 
-
-int disjoint(BitSet s1,BitSet s2){
-	
-	
+int disjoint(BitSet s1, BitSet s2) {
+    int i;
+    int result = 1;
+    for(i = 0; i < MAX; i++) {
+        if (s1[i] && s2[i]) {
+            result = 0; // false, not disjoint
+            break;
+        }
+    }
+    return result; // true, disjoint
 }
 
-
-int equal(BitSet s1,BitSet s2){
-	
-	
+int equal(BitSet s1, BitSet s2) {
+    int i, result = 1;
+    for(i = 0; i < MAX; i++) {
+        if (s1[i] != s2[i]) {
+            result = 0; // false, not equal
+            break;
+        }
+    }
+    return result; // true, equal
 }
 
-
-int cardinality(BitSet s){
-	
-	
+int cardinality(BitSet s) {
+    int i, count = 0;
+    for(i = 0; i < MAX; i++) {
+        if (s[i] == 1) {
+            count++;
+        }
+    }
+    return count;
 }
 
-
-int subset(BitSet s1,BitSet s2){
-	
-	
-	
+int subset(BitSet s1, BitSet s2) {
+    int i, result = 1;
+    for(i = 0; i < MAX; i++) {
+        if (s1[i] && !s2[i]) {
+            result = 0; // false, s1 is not a subset of s2
+            break;
+        }
+    }
+    return result; // true, s1 is a subset of s2
 }
--- main.c
+++ main.c
@@ -7,28 +7,60 @@
 
 int main(int argc, char *argv[]) {
 	
-	BitSet s1;
-	BitSet s2;
-	BitSet result;
-	
-	
-	initialize(s1);
-	initialize(s2);
-	initialize(result);
-	
-	add(5, s1);
-	add(5, s2);
-//	display(s);
+    BitSet s1, s2, result;
 
+    // Initialize sets
+    initialize(s1);
+    initialize(s2);
+    initialize(result);
 
-//	getUnion(result, s1, s2);
+    // Add elements to s1
+    add(1, s1);
+    add(2, s1);
+    add(3, s1);
+//    printf("s1: ");
+//    display(s1);
 
-	intersection(result, s1, s2);
+    // Add elements to s2
+    add(2, s2);
+    add(3, s2);
+    add(4, s2);
+//    printf("s2: ");
+//    display(s2);
 
-	int i;
-	for(i=0; i<MAX; i++){
-		printf("%d ", result[i]);
-	}
+    // Union of s1 and s2
+    getUnion(result, s1, s2);
+    printf("Union of s1 and s2: ");
+    display(result);
+
+    // Intersection of s1 and s2
+    intersection(result, s1, s2);
+    printf("\nIntersection of s1 and s2: ");
+    display(result);
+
+    // Difference between s1 and s2 (s1 - s2)
+    difference(result, s1, s2);
+    printf("Difference (s1 - s2): ");
+    display(result);
+
+    // Check if s1 is empty
+    printf("Is s1 empty? %s\n", isEmpty(s1) ? "Yes" : "No");
+
+    // Check if s1 contains 3
+    printf("Does s1 contain 3? %s\n", contains(s1, 3) ? "Yes" : "No");
+
+    // Check if s1 and s2 are disjoint
+    printf("Are s1 and s2 disjoint? %s\n", disjoint(s1, s2) ? "Yes" : "No");
+
+    // Check if s1 and s2 are equal
+    printf("Are s1 and s2 equal? %s\n", equal(s1, s2) ? "Yes" : "No");
+
+    // Find the cardinality of s1
+    printf("Cardinality of s1: %d\n", cardinality(s1));
+
+    // Check if s1 is a subset of s2
+    printf("Is s1 a subset of s2? %s\n", subset(s1, s2) ? "Yes" : "No");
+    
 	return 0;
 }
 
